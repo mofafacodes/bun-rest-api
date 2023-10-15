3db6bba53e8615c00561d74378ad3dff167ecc68
Nutifafa Kpodo <nutifafakpodo@outlook.com>
2023-10-15 17:53:52 +0000

    adding api routes and returning data

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,30 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Bun Dev REST-APIs")
 .get('/post/:id', ({params: {id}}) => {return {id: id, title: "Hello Bun Dev REST-APIs"}})
+.post('/post', ({body, set}) =>{ 
+  set.status = 201
+  return {body}})
+.get('/track/*', () => {return "Track Routes"})
+.get('/tracks', () => {
+  // return new Response(JSON.stringify({
+  //   "tracks": [
+  //     'dancing feet',
+  //     'walking toes',
+  //     "running knees"
+  //   ]
+  // }),
+  // {
+  //   headers: {'Content-Type': 'application/json'}}
+  // )
+  return {
+    "tracks":[
+    'dancing feet',
+     'walking toes',
+    "running knees",
+    "running knees"
+    ]
+  }
+})
 .listen(3000);
 
 console.log(
